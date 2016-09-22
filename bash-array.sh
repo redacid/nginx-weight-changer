ec492761e5ca76d1b05350eb19f345c294857d15
redacid <redacid@redacid.org.ua>
2016-09-22 21:55:00 +0300

    dev

--- bash-array.sh
+++ bash-array.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#<hostname> <ssh port> <priority> <backup 0 or 1>
+#<backend hostname> <ssh port> <priority> <backup 0 or 1>
 debug_separator="------------------------------------------------------------------------------------------------------------"
 host_0=("back1" "2222" "1" "1")
 host_1=("back3" "2222" "2" "0")
@@ -10,9 +10,9 @@ BACKARRAY=(
   host_1[@]
   host_2[@]
 )
-
-front_0=("fr1" "22")
-front_1=("fr2" "22")
+#<frontend hostname> <port> <nginx config file>
+front_0=("fr1" "22" "/home/redacid/nginx.conf")
+front_1=("fr2" "22" "/home/redacid/nginx.conf")
 
 FRONTARRAY=(
   front_0[@]
@@ -22,7 +22,7 @@ FRONTARRAY=(
 frscrpath="/scripts/nginx-weight/"
 scriptname="nginx-change-weight.sh"
 #configpath="/etc/nginx/nginx.conf"
-configpath="/home/redacid/nginx.conf"
+#configpath="/home/redacid/nginx.conf"
 
 bcount=${#BACKARRAY[@]}
 for ((i=0; i<$bcount; i++))
@@ -31,6 +31,7 @@ do
   bport=${!BACKARRAY[i]:1:1}
   bprio=${!BACKARRAY[i]:2:1}
   bbackup=${!BACKARRAY[i]:3:1}
+
   echo ${debug_separator}
   echo "${bhost}:${bport} ${bprio} ${bbackup}"
 
@@ -43,11 +44,12 @@ do
         do
             fhost=${!FRONTARRAY[j]:0:1}
             fport=${!FRONTARRAY[j]:1:1}
+            fconfig=${!FRONTARRAY[j]:2:1}
             echo "      ${fhost}:${fport}"
             ssh ${fhost} -p ${fport} "mkdir -p ${frscrpath}"
             scp -P ${fport} ./${scriptname} ${fhost}:${frscrpath}
             ssh ${fhost} -p ${fport} "chmod +x ${frscrpath}${scriptname}"
-            ssh ${fhost} -p ${fport} "${frscrpath}${scriptname} weight ${bhost} ${weight} ${configpath}"
+            ssh ${fhost} -p ${fport} "${frscrpath}${scriptname} weight ${bhost} ${weight} ${fconfig}"
         done
 
 done
