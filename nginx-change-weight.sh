10a3995713be8d0fd1907b6f8bacab928229c98f
redacid <redacid@redacid.org.ua>
2016-09-22 16:50:22 +0300

    Dev

--- nginx-change-weight.sh
+++ nginx-change-weight.sh
@@ -7,7 +7,7 @@ file="nginx.conf"
 debug="0"
 debug_separator="------------------------------------------------------------------------------------------------------------"
 #comment=`nslookup $host | grep "name =" | cut -d"=" -f2`
-comment=`host $host | cut -d " " -f5`
+comment=`host ${host} | cut -d " " -f5`
 
 if [ $# -lt 2 ]; then
     echo usage $0 command host weight
@@ -23,23 +23,23 @@ fi
 case "$1" in
 
         "weight")
-            regexp='(server)(\s+)('$host')(\s+)(weight)(=)(\d+)(\s+)(max_fails)(=)(\d+)(\s+)(fail_timeout)(=)(5)(;)'
-            line=`cat $file | grep -P $regexp | grep $host | sed 's/^[ \t]*//' | grep -v ^"#" |head -n 1`
+            regexp='(server)(\s+)('${host}')(\s+)(weight)(=)(\d+)(\s+)(max_fails)(=)(\d+)(\s+)(fail_timeout)(=)(5)(;)'
+            line=`cat ${file} | grep -P ${regexp} | grep ${host} | sed 's/^[ \t]*//' | grep -v ^"#" |head -n 1`
             newline="server $host weight=$weight max_fails=1 fail_timeout=5; #$comment"
-            sed -i -e "/^[ \t]*#/!s/$line/$newline/g" $file
+            sed -i -e "/^[ \t]*#/!s/$line/$newline/g" ${file}
 
-        if [ $debug -eq 1 ]; then
-            echo $debug_separator
+        if [ ${debug} -eq 1 ]; then
+            echo ${debug_separator}
             #cat $file
-            echo $debug_separator
+            echo ${debug_separator}
             echo "/^[ \t]*#/!s/$line/$newline/g"
-            echo $debug_separator
-            echo "line="$line
-            echo $debug_separator
-            echo "newline="$newline
-            echo $debug_separator
+            echo ${debug_separator}
+            echo "line="${line}
+            echo ${debug_separator}
+            echo "newline="${newline}
+            echo ${debug_separator}
             echo "regexp=(server)(\s+)('$host')(\s+)(weight)(=)(\d+)(\s+)(max_fails)(=)(\d+)(\s+)(fail_timeout)(=)(5)(;)"
-            echo $debug_separator
+            echo ${debug_separator}
         fi     
         ;;
 
