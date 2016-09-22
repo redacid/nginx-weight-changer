21af5263d86634994016ed1f0bd742f0ec823139
redacid <redacid@redacid.org.ua>
2016-09-22 15:12:33 +0300

    Исправлена опечатка

--- bash-array.sh
+++ bash-array.sh
@@ -1,40 +1,34 @@
-#!/usr/bin/env bash
-#<hostname> <ssh port> <priority> <backup 0 or 1>
+#!/bin/bash
 
+bpath=/storage/srvconfs
 
-host_0=("back1" "2222" "1" "1")
-host_1=("back2" "2222" "2" "0")
-host_2=("back3" "2222" "3" "0")
+rsyncopt="-czavPptgo"
+#rsyncopt="-czavPptgo -n"
 
-BACK_ARRAY=(
+host_0=("back1" "2222" "1")
+host_1=("back2" "2222" "2")
+host_2=("back3" "2222" "3")
+host_3=("back4" "2222" "4")
+host_4=("back5" "2222" "5")
+host_5=("back6" "2222" "6")
+host_6=("back7" "2222" "7")
+
+MAIN_ARRAY=(
   host_0[@]
   host_1[@]
   host_2[@]
+  host_3[@]
+  host_4[@]
+  host_5[@]
+  host_6[@]
 )
-
-front_0=("fr1" "22")
-front_1=("fr2" "22")
-
-FRONT_ARRAY=(
-  host_0[@]
-  host_1[@]
-)
-
-BACK_COUNT=${#BACK_ARRAY[@]}
-for ((i=0; i<$BACK_COUNT; i++))
+COUNT=${#MAIN_ARRAY[@]}
+for ((i=0; i<$COUNT; i++))
 do
-  HOST=${!BACK_ARRAY[i]:0:1}
-  PORT=${!BACK_ARRAY[i]:1:1}
-  PRIO=${!BACK_ARRAY[i]:2:1}
-  BACKUP=${!BACK_ARRAY[i]:3:1}
+  HOST=${!MAIN_ARRAY[i]:0:1}
+  PORT=${!MAIN_ARRAY[i]:1:1}
+  PRIO=${!MAIN_ARRAY[i]:2:1}
+  #echo "------------------------Backup ${HOST}:${PORT}------------------------"
 
-  echo "${HOST}:${PORT} ${PRIO} ${BACKUP}"
-       FRONT_COUNT=${#FRONT_ARRAY[@]}
-        for ((i=0; i<$FRONT_COUNT; i++))
-        do
-              F_HOST=${!FRONT_ARRAY[i]:0:1}
-              F_PORT=${!FRONT_ARRAY[i]:1:1}
-              echo "---${F_HOST}:${F_PORT}"
-        done
-
+  echo "${HOST}:${PORT} ${PRIO}"
 done
