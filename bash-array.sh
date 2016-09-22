107f6f587958c685cae27c332636cf1b141ade90
redacid <redacid@redacid.org.ua>
2016-09-22 15:21:22 +0300

    Рекурсия

--- bash-array.sh
+++ bash-array.sh
@@ -1,34 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#<hostname> <ssh port> <priority> <backup 0 or 1>
 
-bpath=/storage/srvconfs
+host_0=("back1" "2222" "1" "1")
+host_1=("back2" "2222" "2" "0")
+host_2=("back3" "2222" "3" "0")
 
-rsyncopt="-czavPptgo"
-#rsyncopt="-czavPptgo -n"
-
-host_0=("back1" "2222" "1")
-host_1=("back2" "2222" "2")
-host_2=("back3" "2222" "3")
-host_3=("back4" "2222" "4")
-host_4=("back5" "2222" "5")
-host_5=("back6" "2222" "6")
-host_6=("back7" "2222" "7")
-
-MAIN_ARRAY=(
+BACKARRAY=(
   host_0[@]
   host_1[@]
   host_2[@]
-  host_3[@]
-  host_4[@]
-  host_5[@]
-  host_6[@]
 )
-COUNT=${#MAIN_ARRAY[@]}
-for ((i=0; i<$COUNT; i++))
-do
-  HOST=${!MAIN_ARRAY[i]:0:1}
-  PORT=${!MAIN_ARRAY[i]:1:1}
-  PRIO=${!MAIN_ARRAY[i]:2:1}
-  #echo "------------------------Backup ${HOST}:${PORT}------------------------"
 
-  echo "${HOST}:${PORT} ${PRIO}"
+front_0=("fr1" "22")
+front_1=("fr2" "22")
+
+FRONTARRAY=(
+  front_0[@]
+  front_1[@]
+)
+
+bcount=${#BACKARRAY[@]}
+for ((i=0; i<$bcount; i++))
+do
+  bhost=${!BACK_ARRAY[i]:0:1}
+  bport=${!BACK_ARRAY[i]:1:1}
+  bprio=${!BACK_ARRAY[i]:2:1}
+  bbackup=${!BACK_ARRAY[i]:3:1}
+
+  echo "${bhost}:${bport} ${bprio} ${bbackup}"
+       frcount=${#FRONTARRAY[@]}
+        for ((i=0; i<$frcount; i++))
+        do
+              fhost=${!FRONTARRAY[i]:0:1}
+              fport=${!FRONTARRAY[i]:1:1}
+              echo "---${fhost}:${fport}"
+        done
+
 done
