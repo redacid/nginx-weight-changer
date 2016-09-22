71f027f1259f319321936592d64b4dffc78b7627
redacid <redacid@redacid.org.ua>
2016-09-22 13:25:57 +0300

    Modify get-load.sh, add total cpu usage

--- get-load.sh
+++ get-load.sh
@@ -8,24 +8,36 @@ host3="back4"
 mib_cpu_system='.1.3.6.1.4.1.2021.11.52.0'
 mib_cpu_user='.1.3.6.1.4.1.2021.11.50.0'
 mib_cpu_nice='.1.3.6.1.4.1.2021.11.51.0'
-mib_percent_cpu='.1.3.6.1.4.1.2021.11.9.0'
+
+mib_percent_cpu_sys='.1.3.6.1.4.1.2021.11.9.0'
+mib_percent_cpu_usr='.1.3.6.1.4.1.2021.11.10.0'
+mib_percent_cpu_idle='.1.3.6.1.4.1.2021.11.11.0'
+
 
 #val_system=`snmpwalk -v2c -c public $host $mib_cpu_system | cut -d " " -f 4`
 #val_user=`snmpwalk -v2c -c public $host  $mib_cpu_user | cut -d " " -f 4`
 #val_nice=`snmpwalk -v2c -c public $host $mib_cpu_nice | cut -d " " -f 4`
 
-val_cpu1=`snmpwalk -v2c -c public $host1 $mib_percent_cpu | cut -d " " -f 4`
-val_cpu2=`snmpwalk -v2c -c public $host2 $mib_percent_cpu | cut -d " " -f 4`
-val_cpu3=`snmpwalk -v2c -c public $host3 $mib_percent_cpu | cut -d " " -f 4`
+val_cpu_sys1=`snmpwalk -v2c -c public $host1 $mib_percent_cpu_sys | cut -d " " -f 4`
+val_cpu_usr1=`snmpwalk -v2c -c public $host1 $mib_percent_cpu_usr | cut -d " " -f 4`
+let "total_cpu1 = val_cpu_sys1+val_cpu_usr1"
+
+val_cpu_sys2=`snmpwalk -v2c -c public $host2 $mib_percent_cpu_sys | cut -d " " -f 4`
+val_cpu_usr2=`snmpwalk -v2c -c public $host2 $mib_percent_cpu_usr | cut -d " " -f 4`
+let "total_cpu2 = val_cpu_sys2+val_cpu_usr2"
+
+val_cpu_sys3=`snmpwalk -v2c -c public $host3 $mib_percent_cpu_sys | cut -d " " -f 4`
+val_cpu_usr3=`snmpwalk -v2c -c public $host3 $mib_percent_cpu_usr | cut -d " " -f 4`
+let "total_cpu3 = val_cpu_sys3+val_cpu_usr3"
 
 
-#let "total = val_system+val_user+val_nice"
+#let "total1 = val_system+val_user+val_nice"
 
 if [ $debug -eq 1 ]; then
 
-echo "host:$host1 val_cpu=$val_cpu1"
-echo "host:$host2 val_cpu=$val_cpu2"
-echo "host:$host3 val_cpu=$val_cpu3"
+echo "host:$host1 $total_cpu1"
+echo "host:$host2 $total_cpu2"
+echo "host:$host3 $total_cpu3"
 
 
 #echo "val_system="$val_system
