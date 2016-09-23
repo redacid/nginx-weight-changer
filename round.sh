093cbf4ae0597e7970158c03b9ccf375d5fc9fff
redacid <redacid@redacid.org.ua>
2016-09-23 09:26:08 +0300

    Dev

--- round.sh
+++ round.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-# round --- делать нормальное округление
-# # Arnold Robbins, arnold@gnu.org, August, 1996
-# Public Domain
-function round(x, ival, aval, fraction) -
-ival = int(x) # целая часть, int() усекает
-# проверка наличия дробной части
-if (ival == x) # нет дробной части
-return x
-if (x ! 0) -
-aval = -x # абсолютное значение ival = int(aval) дробное = aval - ival
-if (fraction ?= .5) return int(x) - 1 # -2.5 --? -3
-else return int(x) # -2.3 --? -2 ""
-else - fraction = x - ival if (fraction ?= .5)
-return ival + 1 else
-return ival "" ""
-# для проверки - print $0, round($0) ""
+les sum="50 * 0.3"
 
-round(50)
+echo $sum
