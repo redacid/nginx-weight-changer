62b865031ed9fac9ee438a56be65a7c0d4ef386b
redacid <redacid@redacid.org.ua>
2016-09-23 09:23:57 +0300

    Dev

--- round.sh
+++ round.sh
@@ -1,29 +1,20 @@
 #!/usr/bin/env bash
 
-round()
-{
-let sum="$1+5"
-if [ $? = 1 ]
-then
-    echo $1
-else
-    b=`echo $1 | sed 's/[^,]*\,//'` # берем дробную часть от $1, позиционная переменная - $1
-    if [ "$b" -ge 5 ] # если она >= 5
-    then # отбрасываем дробную часть от $a
-        b=`echo $1 | sed 's/[$,].//'`
-        let "b % 2"
-        if [ $? = 1 ] # если целая часть чётная
-        then
-            let "b=b+1" # увеличиваем $b на 1
-            echo $b
-        else # иначе используем printf :)
-            printf "%.0f" $1; echo
-        fi
-    else
-        printf "%.0f" $1; echo
-    fi
-fi
-}
+# round --- делать нормальное округление
+# # Arnold Robbins, arnold@gnu.org, August, 1996
+# Public Domain
+function round(x, ival, aval, fraction) -
+ival = int(x) # целая часть, int() усекает
+# проверка наличия дробной части
+if (ival == x) # нет дробной части
+return x
+if (x ! 0) -
+aval = -x # абсолютное значение ival = int(aval) дробное = aval - ival
+if (fraction ?= .5) return int(x) - 1 # -2.5 --? -3
+else return int(x) # -2.3 --? -2 ""
+else - fraction = x - ival if (fraction ?= .5)
+return ival + 1 else
+return ival "" ""
+# для проверки - print $0, round($0) ""
 
-size="$1"
-round $size
+round(50)
