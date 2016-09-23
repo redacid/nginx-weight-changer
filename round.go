b5009eb40100c4305a2e5e96e9d2efc24e2d0a3b
redacid <redacid@redacid.org.ua>
2016-09-23 10:59:05 +0300

    Dev

--- round.go
+++ round.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"flag"
+	"regexp"
+	"os"
+	"bufio"
 )
 
 var command string
+var strForGrep string
+var fileForGrep string
 var floatForRound float64
 
 //var command = flag.String("command", "round", "Комманда(round...)")
@@ -13,8 +18,10 @@ var floatForRound float64
 //var ip = flag.Int("flagname", 1234, "help message for flagname")
 
 func init() {
-	flag.StringVar(&command, "c", command, "Комманда(round...)")
-	flag.Float64Var (&floatForRound, "round", floatForRound, "Округлить до целого")
+	flag.StringVar(&command, "c", command, "Комманда(round,grep ...)")
+	flag.Float64Var (&floatForRound, "round", floatForRound, "Число для округления до целого")
+	flag.StringVar (&strForGrep, "grep", strForGrep, "Строка(regex) для grep фильтра")
+	flag.StringVar (&fileForGrep, "grepfile", strForGrep, "Файл для grep фильтра")
 }
 
 func round(f float64) int {
@@ -27,6 +34,70 @@ func round(f float64) int {
 	return 0
 }
 
+func grep(re, filename string) {
+	regex, err := regexp.Compile(re)
+	if err != nil {
+		return // there was a problem with the regular expression.
+	}
+
+	fh, err := os.Open(filename)
+	f := bufio.NewReader(fh)
+
+	if err != nil {
+		return // there was a problem opening the file.
+	}
+	defer fh.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		buf, _ , err = f.ReadLine()
+		if err != nil {
+			return
+		}
+
+		s := string(buf)
+		if regex.MatchString(s) {
+			fmt.Printf("%s\n", string(buf))
+		}
+	}
+}
+
+func replace(re, repl, filename string) {
+	regex, err := regexp.Compile(re)
+	if err != nil {
+		return // there was a problem with the regular expression.
+	}
+
+	fh, err := os.Open(filename)
+	f := bufio.NewReader(fh)
+
+	if err != nil {
+		return // there was a problem opening the file.
+	}
+	defer fh.Close()
+
+	buf := make([]byte, 1024)
+	for {
+		buf, _ , err = f.ReadLine()
+		if err != nil {
+			return
+		}
+
+		s := string(buf)
+		result := regex.ReplaceAllString(s, repl)
+		fmt.Print(result + "\n")
+	}
+}
+
+//func main() {
+//	flag.Parse()
+//	if flag.NArg() == 3 {
+//		repl(flag.Arg(0), flag.Arg(1), flag.Arg(2))
+//	} else {
+//		fmt.Printf("Wrong number of arguments.\n")
+//	}
+//}
+
 func main() {
 	flag.Parse()
 	switch {
@@ -34,5 +105,8 @@ func main() {
 		fmt.Printf("%s", "Не указана или неверная комманда введите -h для получения помощи\n")
 	case command == "round":
 		fmt.Printf("%d", round(floatForRound))
+	case command == "grep":
+		//fmt.Printf("%d", round(floatForRound))
+		grep(strForGrep, fileForGrep)
 	}
 }
